ccaf0ae846ea8b7d07ca2481b4f001fc29b7d90e
Your Name <you@example.com>
2022-06-03 16:39:06 -0400

    Icon Classes

--- src/components/MapaComponent.vue
+++ src/components/MapaComponent.vue
@@ -22,14 +22,50 @@ export default {
         maxZoom: 19,
         attribution: "© OpenStreetMap",
       }).addTo(this.map);
-      const leafletIcon = L.icon({
-        iconUrl: "https://cdn-icons-png.flaticon.com/512/856/856332.png",
-        iconSize: [45, 60],
-        iconAnchor: [22, 94],
-        popupAnchor:[12,-90]
+      // const leafletIcon = L.icon({
+      //   iconUrl: "https://cdn-icons-png.flaticon.com/512/856/856332.png",
+      //   iconSize: [45, 60],
+      //   iconAnchor: [22, 94],
+      //   popupAnchor: [12, -90],
+      // });
+      const LeafletIcon = L.Icon.extend({
+        options: {
+          iconUrl: "",
+          iconSize: [38, 95],
+          iconAnchor: [22, 94],
+          popuAnchor: [-3, -76],
+        },
       });
-      const marker = L.marker([-3.042602743486333, -59.95165651808512], {
-        icon: leafletIcon,
+      // const myGeoJSON = {
+      //   "type": "Feature",
+      //   "properties": {
+      //     "name": "Arena da Amazônia",
+      //     "amenity": "Estádio de Futebol",
+      //     "popuContent": "Aqui é onde o Manaus FC joga",
+      //   },
+      //   "geometry": {
+      //     "type": "LineString",
+      //     "coordinates": [-3.0830100050083993, -60.0281392021517]
+      //   }
+      // };
+      // L.geoJSON(myGeoJSON).addTo(this.map);
+      const firstIcon = new LeafletIcon({
+        iconUrl: "https://cdn-icons-png.flaticon.com/512/856/856332.png",
+      });
+      const secondIcon = new LeafletIcon({
+        iconUrl: "https://cdn-icons-png.flaticon.com/512/2702/2702604.png",
+      });
+      const thirdIcon = new LeafletIcon({
+        iconUrl: "https://cdn-icons-png.flaticon.com/512/4891/4891726.png",
+      });
+      const marker_1 = L.marker([-3.042602743486333, -59.95165651808512], {
+        icon: firstIcon,
+      }).addTo(this.map);
+      const marker_2 = L.marker([-3.1270460904142587, -60.00549721121329], {
+        icon: secondIcon,
+      }).addTo(this.map);
+      const marker_3 = L.marker([-3.0640370911474673, -60.10381728905795], {
+        icon: thirdIcon,
       }).addTo(this.map);
       const circle = L.circle([-3.0781166984188983, -60.00676763513346], {
         color: "red",
@@ -42,7 +78,9 @@ export default {
         [-3.081323973313841, -59.9515710625983],
         [-3.1017196921402, -59.95683981683938],
       ]).addTo(this.map);
-      marker.bindPopup("Eu sou um marcador").openPopup();
+      marker_1.bindPopup("Manaus");
+      marker_2.bindPopup("Terminal 2");
+      marker_3.bindPopup("Praia da Ponta Negra");
       circle.bindPopup("Eu sou um círculo");
       polygon.bindPopup("Eu sou um poligono");
     },
